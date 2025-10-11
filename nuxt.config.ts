1d6384772d18d87123cdc36028199712a21e1beb
NoahOnFyre <noahonfyre@gmail.com>
2025-10-11 21:34:05 +0200

    feat: configure tailwind and set up theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/image']
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    modules: ['@nuxt/fonts', '@nuxt/image'],
+    css: ['~/assets/css/main.css'],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ]
+    }
 })
