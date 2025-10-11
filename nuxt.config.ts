ec27f36c818e952138d8105ca4ef9fd1a810f423
NoahOnFyre <noahonfyre@gmail.com>
2025-10-11 22:56:17 +0200

    fix: remove nuxt/fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
-    modules: ['@nuxt/fonts', '@nuxt/image'],
+    modules: ['@nuxt/image'],
     css: ['~/assets/css/main.css'],
     vite: {
         plugins: [
