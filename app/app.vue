d351d8ea4d2a6a3734b1e37d86dc15dc20d2271a
NoahOnFyre <noahonfyre@gmail.com>
2025-10-12 23:08:48 +0200

    feat: add custom tracking to subtitle

--- app/app.vue
+++ app/app.vue
@@ -7,7 +7,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
           </svg>
-          <h1 class="font-display font-bold text-4xl">HESSEN, GERMANY</h1>
+          <h1 class="font-display font-bold text-4xl tracking-tighter">HESSEN, GERMANY</h1>
         </div>
         <h1 class="font-display font-black text-9xl tracking-tighter">FRANKFURT</h1>
       </div>
