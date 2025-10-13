b7659cc974ce96d5f6a2617d9b34ddda9b9a9bb1
NoahOnFyre <noahonfyre@gmail.com>
2025-10-14 01:30:32 +0200

    feat: add navbar and landing section to index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,9 @@
 <template>
+  <div class="h-screen bg-volt-600 flex flex-col px-12 pb-12">
+    <Navbar></Navbar>
+    <LandingSection></LandingSection>
+  </div>
   <FrankfurtSection></FrankfurtSection>
 </template>
+<script setup lang="ts">
+</script>
