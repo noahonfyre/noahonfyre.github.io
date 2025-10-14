33ea8c154197541608fd32a54641056ddcc169e1
NoahOnFyre <noahonfyre@gmail.com>
2025-10-15 00:05:36 +0200

    refactor: adjust logo size

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="grid grid-flow-col py-6">
     <div class="flex items-center h-full">
       <NuxtLink to="/">
-        <svg viewBox="0 0 18 8" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-16">
+        <svg viewBox="0 0 18 8" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-8">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M0.5 8L8.5 0L8.5 8H6.5V4.5L3 8H0.5Z" fill="currentColor"/>
         <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5 0L9.5 8L9.5 -6.99382e-07L11.5 -5.24537e-07L11.5 3.5L15 -2.18557e-07L17.5 0Z" fill="currentColor"/>
       </svg>
