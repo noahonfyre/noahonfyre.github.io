eb9b0f04bfc8596c119fc880de6e0b4a1edf8ba3
NoahOnFyre <noahonfyre@gmail.com>
2025-10-14 02:15:38 +0200

    revert: remove banner

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,4 @@
 <template>
-  <section class="bg-red-600 text-white px-12 py-6 text-center">
-    Please take into consideration that this website is not finished and its contents are subject to change.
-  </section>
   <div class="h-screen bg-volt-600 flex flex-col px-12 pb-12">
     <Navbar></Navbar>
     <LandingSection></LandingSection>
