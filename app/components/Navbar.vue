6a55c062cb535f4abbc9f4bcbc49987d32237f84
NoahOnFyre <noahonfyre@gmail.com>
2025-10-14 22:57:31 +0200

    refactor: slightly adjust layout

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
-  <nav class="grid grid-flow-col py-8">
-    <div>
+  <nav class="grid grid-flow-col py-6">
+    <div class="flex items-center h-full">
       <NuxtLink to="/">
-        <svg viewBox="0 0 18 8" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6">
+        <svg viewBox="0 0 18 8" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-16">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M0.5 8L8.5 0L8.5 8H6.5V4.5L3 8H0.5Z" fill="currentColor"/>
         <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5 0L9.5 8L9.5 -6.99382e-07L11.5 -5.24537e-07L11.5 3.5L15 -2.18557e-07L17.5 0Z" fill="currentColor"/>
       </svg>
       </NuxtLink>
     </div>
 
-    <div class="flex justify-end items-center gap-12">
-      <NuxtLink class="font-display font-bold" v-for="item in navItems" :to="item.to">{{ item.text }}</NuxtLink>
+    <div class="flex justify-end items-center">
+      <NuxtLink class="font-display font-bold py-2 px-6 hover:text-element-950 ease-in-out duration-150" v-for="item in navItems" :to="item.to">{{ item.text }}</NuxtLink>
     </div>
   </nav>
 </template>
