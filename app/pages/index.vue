229c2cf7a3512bbe19ef9c3bf98f6a49c27eb992
NoahOnFyre <noahonfyre@gmail.com>
2025-10-15 01:37:18 +0200

    chore: remove nuxt stuff

--- app/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="h-screen bg-volt-600 flex flex-col px-6 pb-6">
-    <Navbar></Navbar>
-    <LandingSection></LandingSection>
-  </div>
-  <FrankfurtSection></FrankfurtSection>
-</template>
